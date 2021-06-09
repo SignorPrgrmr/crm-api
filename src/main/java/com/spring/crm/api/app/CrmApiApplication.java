adff9756d321558eab9d57a566dfd116a143a1c8
amin <signorprgrmr@gmail.com>
2021-06-09 12:43:11 +0430

    Defining datasource

--- src/main/java/com/spring/crm/api/app/CrmApiApplication.java
+++ src/main/java/com/spring/crm/api/app/CrmApiApplication.java
@@ -1,9 +1,11 @@
-package com.spring.crm.api;
+package com.spring.crm.api.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.spring.crm.api")
 public class CrmApiApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/spring/crm/api/config/DefiningDatasource.java
@@ -0,0 +1,55 @@
+package com.spring.crm.api.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class DefiningDatasource {
+
+    private final Environment env;
+
+    @Autowired
+    public DefiningDatasource(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("database.driver_class"));
+        dataSource.setUrl(env.getProperty("database.url"));
+        dataSource.setUsername(env.getProperty("database.username"));
+        dataSource.setPassword(env.getProperty("database.password"));
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        dataSource.setConnectionProperties(properties);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean localSessionFactoryBean() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource());
+        localSessionFactoryBean.setPackagesToScan("com.spring.crm.api.entity");
+        return localSessionFactoryBean;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+        return transactionManager;
+    }
+
+}
