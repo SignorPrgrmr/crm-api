7c86d1f50fcef58c6dbbcee50ee1b9e41d1ddbae
amin <signorprgrmr@gmail.com>
2021-06-13 12:42:03 +0430

    Implementing methods of CustomerDAO

--- src/main/java/com/spring/crm/api/dao/CustomerDAOImpl.java
+++ src/main/java/com/spring/crm/api/dao/CustomerDAOImpl.java
@@ -1,29 +1,52 @@
 package com.spring.crm.api.dao;
 
 import com.spring.crm.api.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public CustomerDAOImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public List<Customer> getCustomers() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("select Customer from Customer", Customer.class).getResultList();
     }
 
     @Override
     public Customer getCustomer(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Customer.class, id);
     }
 
     @Override
     public void saveOrUpdateCustomer(Customer customer) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(customer);
     }
 
     @Override
     public void deleteCustomer(int id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        /*Query<Customer> deleteQuery = session.createQuery("delete from Customer where id = :id", Customer.class);
+        deleteQuery.setParameter("id", id);
+        deleteQuery.executeUpdate();*/
+        Customer customer = session.get(Customer.class, id);
+        if (customer != null) {
+            session.remove(customer);
+        }
     }
 
 }
